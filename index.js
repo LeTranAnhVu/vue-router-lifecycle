9d1f76a414756b3dc852ac391199414a5361655c
LeTranAnhVu <letrananhvu.developer@gmail.com>
2019-03-04 13:35:47 +0700

    update loading-spinner

--- index.js
+++ index.js
@@ -1,76 +1,88 @@
-
 var User = {
   props: ['user'],
   template: `
-    <div class="card" style="width: 18rem;">
+    <div v-if="user" class="card" style="width: 18rem;">
       <img class="card-img-top" :src="user.url" alt="Card image cap">
       <div class="card-body">
         <p class="card-text">{{user.title}}</p>
       </div>
     </div>
   `,
-  mounted: function(){
-    console.log('child-user is already mounted');
-  }
+  mounted: function () {
+    console.log('child-user is already mounted')
+  },
 }
-var Home = {template: "<h1>Home page</h1>"}
+var Home = {template: '<h1>Home page</h1>'}
 
 var Users = {
-  data: function() {
+  data: function () {
     return {
-      users: [],
+      users: null,
+      isLoading: false,
     }
   },
   components: {
-    'component-user': User
+    'component-user': User,
   },
   template: `
-   <section>
+   <section id="section-1">
     <h1>User page</h1>
-    <div class="row">
-      <div class="col-md-4" v-for="item in users">
-        <component-user :user="item"></component-user>
+    
+    <div class="row" v-if="users">
+      <div class="col-md-4" v-for="user in users">
+        <component-user :user="user"></component-user>
       </div>
+    </div>
+    <div class="loading-spinner" v-if="isLoading">
+       <i class="fas fa-spinner"></i>
     </div>
   </section>
   `,
   // life cycle hook
-  created: function(){
-    console.log('users created');
-    axios.get('https://jsonplaceholder.typicode.com/photos')
-      .then(res => {
-        console.log('fetched data successfully', res.data);
-        this.users = res.data;
+  created: function () {
+    console.log('created')
+    this.isLoading = true
+    axios.get('https://jsonplaceholder.typicode.com/photos').then(res => {
+      console.log('fetched data successfully', res.data)
+      this.users = res.data
     })
   },
   mounted: function () {
+    console.log('parent users is mounted')
+  },
+  updated: function () {
     this.$nextTick(function () {
-      console.log('parent users is mounted')
+      console.log('parent - updation complete - n')
+      this.isLoading = false
     })
-  }
+  },
 }
-
 
 var routes = [
   {path: '', component: Home},
-  {path: '/user', component: Users}
-];
+  {path: '/user', component: Users},
+]
 
 var app = new Vue({
   el: '#app',
   components: {
-    Users
+    Users,
   },
   router: new VueRouter({
     routes,
   }),
   methods: {
-    moveToUser: function(){
-      this.$router.push('/user');
-    }
+    moveToUser: function () {
+      this.$router.push('/user')
+    },
   },
-  mounted: function(){
-    console.log('app is already mounted');
-  }
+  mounted: function () {
+    console.log('app is already mounted')
+  },
+  updated: function () {
+    this.$nextTick(function () {
+      console.log('app - updation complete')
+    })
+  },
 })
 
